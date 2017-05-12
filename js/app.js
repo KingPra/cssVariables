e166f21d4884da03502b4dd5910641a8779378e0
KingPra <kingi.pra@gmail.com>
2017-05-11 21:08:06 -0400

    cleaning up file

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-const inputs = document.querySelectorAll('.controls input');
-console.log('testing this');
+document.addEventListener('DOMContentLoaded',(event) => {
+  const inputs = document.querySelectorAll('.controls input');
+
+  inputs.forEach(input => input.addEventListener('change', handleUpdate));
+  inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+  });
+
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
-  document.documentElement.style.setProperty(`-${this.name}`, this.value + suffix);
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
-
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
